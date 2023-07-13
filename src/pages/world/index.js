ed8c1ff188b022895061add2a4d0ddcca0d986d1
Andrew Ratnikov <andrew.ratnikov@guesty.com>
2023-07-13 12:15:50 +0300

    fix

--- src/pages/world/index.js
+++ src/pages/world/index.js
@@ -34,7 +34,9 @@ const World = () => {
     }
 
     if (total) {
-      const newData = data.filter((item) => item.averagePropertyCost <= total);
+      const newData = initialData.filter(
+        (item) => item.averagePropertyCost <= total
+      );
 
       setData(newData);
     } else {
